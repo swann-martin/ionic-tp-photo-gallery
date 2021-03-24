90a264ef590cbc1b4a9d5e6cee8f386cec7c15ae
swann-martin <swannmartin1988@gmail.com>
2021-03-24 15:59:15 +0100

    adds the function to save pictures in an array and retrieve the saved pictures then to display them

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -16,19 +16,76 @@ const { Camera, Filesystem, Storage } = Plugins;
 })
 export class PhotoService {
   public photos: Photo[] = [];
+  private PHOTO_STORAGE: string = 'photos';
+
   constructor() {}
 
   public async addNewToGallery() {
+    // takes the picture
     const capturedPhoto = await Camera.getPhoto({
       resultType: CameraResultType.Uri,
       source: CameraSource.Camera,
       quality: 100,
     });
 
-    this.photos.unshift({
-      filepath: 'soon...',
-      webviewPath: capturedPhoto.webPath,
+    const savedImageFile = await this.savePicture(capturedPhoto);
+    this.photos.unshift(savedImageFile);
+
+    Storage.set({
+      key: this.PHOTO_STORAGE,
+      value: JSON.stringify(this.photos),
     });
+  }
+
+  private async savePicture(cameraPhoto: CameraPhoto) {
+    // convert photo to base 64 format for the API we run to save
+    const base64Data = await this.readAsBase64(cameraPhoto);
+
+    const fileName = new Date().getTime() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: FilesystemDirectory.Data,
+    });
+
+    return {
+      filepath: fileName,
+      webviewPath: cameraPhoto.webPath,
+    };
+  }
+
+  private async readAsBase64(cameraPhoto: CameraPhoto) {
+    // it will get the picture read it as a blob and transform it to a base64 format
+    const response = await fetch(cameraPhoto.webPath!);
+    const blob = await response.blob();
+
+    return (await this.convertBlobToBase64(blob)) as string;
+  }
+
+  convertBlobToBase64 = (blob: Blob) =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+      reader.readAsDataURL(blob);
+    });
+
+  public async loadSaved() {
+    // gets a specific photo from the array data thanks to its key
+    const photoList = await Storage.get({ key: this.PHOTO_STORAGE });
+    this.photos = JSON.parse(photoList.value) || [];
+
+    for (let photo of this.photos) {
+      const readFile = await Filesystem.readFile({
+        path: photo.filepath,
+        directory: FilesystemDirectory.Data,
+      });
+
+      // for webplatform only
+      photo.webviewPath = `data.image/jpeg;base64, ${readFile.data}`;
+    }
   }
 }
 
